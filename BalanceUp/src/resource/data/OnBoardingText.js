3f68f4e89808fe7a518df91d337ef7df87fdae1d
hyeone999 <caps026@naver.com>
2023-05-01 13:44:14 +0900

    refactor: 온보딩 화면 (불필요한 코드 삭제 및 코드 최적화)

---
+++ BalanceUp/src/resource/data/OnBoardingText.js
@@ -0,0 +1,27 @@
+import OnBoarding_1 from '../image/Onboarding/onboarding-1.png';
+import OnBoarding_2 from '../image/Onboarding/onboarding-2.png';
+import OnBoarding_3 from '../image/Onboarding/onboarding-3.png';
+
+export const TextData = [
+  {
+    id: 1,
+    img: OnBoarding_1,
+    title: '성취감을 느끼며 루틴 형성',
+    text1: '소소한 성취감을 2주 간 느끼며',
+    text2: '자연스러운 루틴형성을 도와드려요',
+  },
+  {
+    id: 2,
+    img: OnBoarding_2,
+    title: '나만의 루틴으로 캐릭터 성장',
+    text1: '루틴을 성공할 때마다 얻는',
+    text2: 'RP(Routine Point)로 내 캐릭터를 성장시켜요',
+  },
+  {
+    id: 3,
+    img: OnBoarding_3,
+    title: '원하는 시간에 알림',
+    text1: '내가 원하는 시간에 알림을 설정하여',
+    text2: '루틴을 잊지 않을 수 있어요',
+  },
+];
--- BalanceUp/src/screens/Login/OnBoarding.js
+++ BalanceUp/src/screens/Login/OnBoarding.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import {
   responsiveFontSize,
@@ -12,23 +12,22 @@ import FastImage from 'react-native-fast-image';
 import {getRefreshToken} from '../../actions/memberJoinApi';
 import {deleteExpiredRoutine} from '../../actions/routineAPI';
 import {jwtState, jwtRefreshState} from '../../recoil/atom';
+import {TextData} from '../../resource/data/OnBoardingText';
 
 import jwt_decode from 'jwt-decode';
 import KeyumTypo from '../../resource/image/KeyumLOGOTYPO.png';
-import OnBoarding_1 from '../../resource/image/Onboarding/onboarding-1.png';
-import OnBoarding_2 from '../../resource/image/Onboarding/onboarding-2.png';
-import OnBoarding_3 from '../../resource/image/Onboarding/onboarding-3.png';
 import NextBtn from '../../resource/image/Onboarding/NextBtn.svg';
 import PrevBtn from '../../resource/image/Onboarding/PrevBtn.svg';
 
 export default function OnBoarding({navigation}) {
-  const [jwt, setjwt] = useRecoilState(jwtState);
-  const [jwtRefresh, setJwtRefresh] = useRecoilState(jwtRefreshState);
+  const [setjwt] = useRecoilState(jwtState);
+  const [setJwtRefresh] = useRecoilState(jwtRefreshState);
 
-  React.useEffect(() => {
+  useEffect(() => {
     checkJwt('jwt');
-  }, []);
-  const getData = async (key: string) => {
+  });
+
+  const getData = async key => {
     try {
       const value = await AsyncStorage.getItem(key);
       if (value !== null) {
@@ -40,7 +39,7 @@ export default function OnBoarding({navigation}) {
     }
   };
 
-  const checkJwt = async (key: string) => {
+  const checkJwt = async key => {
     try {
       const dataToken = await getData('jwt');
       const dataRefreshToekn = await getData('jwtRefresh');
@@ -77,40 +76,9 @@ export default function OnBoarding({navigation}) {
   };
 
   const [btnDisabled, setBtnDisabled] = useState(true);
-  const TextData = [
-    {
-      id: 1,
-      img: OnBoarding_1,
-      title: '성취감을 느끼며 루틴 형성',
-      text1: '소소한 성취감을 2주 간 느끼며',
-      text2: '자연스러운 루틴형성을 도와드려요',
-    },
-    {
-      id: 2,
-      img: OnBoarding_2,
-      title: '나만의 루틴으로 캐릭터 성장',
-      text1: '루틴을 성공할 때마다 얻는',
-      text2: 'RP(Routine Point)로 내 캐릭터를 성장시켜요',
-    },
-    {
-      id: 3,
-      img: OnBoarding_3,
-      title: '원하는 시간에 알림',
-      text1: '내가 원하는 시간에 알림을 설정하여',
-      text2: '루틴을 잊지 않을 수 있어요',
-    },
-  ];
-
-  const goLogin = () => {
-    navigation.navigate('Login');
-  };
 
   const handleChange = index => {
-    if (index === 2) {
-      setBtnDisabled(false);
-    } else {
-      setBtnDisabled(true);
-    }
+    index === 2 ? setBtnDisabled(false) : setBtnDisabled(true);
   };
 
   return (
@@ -129,13 +97,7 @@ export default function OnBoarding({navigation}) {
         showsButtons={true}>
         {TextData.map(data => (
           <View style={styles.slide} key={data.id}>
-            <FastImage
-              style={[
-                styles.onBoardingIMG,
-                {marginRight: data.id === 3 ? responsiveWidth(2) : null},
-              ]}
-              source={data.img}
-            />
+            <FastImage style={styles.onBoardingIMG} source={data.img} />
             <Text style={styles.title}>{data.title}</Text>
             <Text style={styles.subTitle}>{data.text1}</Text>
             <Text style={styles.subTitle}>{data.text2}</Text>
@@ -147,7 +109,7 @@ export default function OnBoarding({navigation}) {
           styles.btnStart,
           {backgroundColor: btnDisabled ? '#CED6FF' : '#585FFF'},
         ]}
-        onPress={goLogin}
+        onPress={() => navigation.navigate('Login')}
         disabled={btnDisabled}
         activeOpacity={1.0}>
         <Text style={styles.btnText}>시작하기</Text>
@@ -177,7 +139,7 @@ const styles = StyleSheet.create({
   onBoardingIMG: {
     width: responsiveWidth(58),
     height: responsiveHeight(36),
-    marginLeft: 12,
+    marginLeft: 10,
     marginBottom: responsiveHeight(3),
   },
   title: {
