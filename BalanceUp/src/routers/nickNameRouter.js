7fbf2a2187f396fe846e82cca782ff5773996bb5
hyeone999 <caps026@naver.com>
2023-05-04 14:42:49 +0900

    modify: nickNameRouter 주석 추가 및 불필요한 코드 삭제

--- BalanceUp/src/routers/nickNameRouter.js
+++ BalanceUp/src/routers/nickNameRouter.js
@@ -3,28 +3,11 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import NameScreen from '../screens/register/NameScreen';
 import AgreeScreen from '../screens/register/AgreeScreen';
-import MainScreen from '../screens/home/MainScreen';
 import {MainRouter} from './mainRouter';
 const NickNameStack = createStackNavigator();
 
-// const routeInfo = [
-//   {
-//     name: "Name",
-//     component: NameScreen,
-//   },
-//   {
-//     name: "Agree",
-//     component:AgreeScreen,
-//   },
-//   {
-//     name: "UseInfo",
-//     component: UseInfoScreen,
-//   },
-//   {
-//     name: "ServiceInfo",
-//     component: ServiceInfoScreen,
-//   },
-// ];
+// NickNameRouter : 로그인 화면에서 넘어오는 페이지
+// flow : Name -> Agree -> MainRouter
 
 export function NickNameRouter() {
   return (
@@ -49,16 +32,3 @@ export function NickNameRouter() {
     </NavigationContainer>
   );
 }
-// const NickNameRouter=[];
-
-// routeInfo.map(res=>{
-//   NickNameRouter.push(
-//     <NickNameStack.Screen
-//       key={res.name}
-//       name={res.name}
-//       component={res.component}
-//     />
-//   )
-// })
-
-exports.defulat = NickNameRouter;
