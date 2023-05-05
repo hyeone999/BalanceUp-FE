ef3b7bd514d70fb24447a224c5afc69b70c6b45b
hyeone999 <caps026@naver.com>
2023-05-05 18:27:21 +0900

    modify: 초기 Router 파일명 수정, App.js 변경사항 적용

--- BalanceUp/App.js
+++ BalanceUp/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import {HomeStackScreen} from './src/routers';
+import {HomeStackScreen} from './src/routers/onBoardingRouter';
 import {RecoilRoot} from 'recoil';
 import SplashScreen from 'react-native-splash-screen'; /** 추가 **/
 
@@ -9,7 +9,7 @@ function App() {
     try {
       setTimeout(() => {
         SplashScreen.hide(); /** 추가 **/
-      }, 3000); /** 스플래시 시간 조절 (2초) **/
+      }, 3000); /** 스플래시 시간 조절 (3초) **/
     } catch (e) {
       console.warn('에러발생');
       console.warn(e);
